b421f2c7c07aa5b5eb49f6d626e9ab68bd6167d9
Magical Glove <90389752+MagicalGlove@users.noreply.github.com>
2025-06-04 15:11:33 +0200

    Adds cypress test for "Bid manager" flow

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,13 @@
-const { defineConfig } = require("cypress");
+import { defineConfig } from "cypress";
 
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
+    supportFile: "cypress/support/e2e.js",
     baseUrl: "http://localhost:5173",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    viewportWidth: 1200,
+    viewportWidth: 2000,
     viewportHeight: 800,
   },
 });
